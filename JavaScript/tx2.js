133bb177f0620ce12667c8f047c6c825eb0112f1
Anderson Postigo <anderson.postigo0@gmail.com>
2026-06-08 22:37:15 -0300

    atualizado junho

---
+++ JavaScript/tx2.js
@@ -0,0 +1,46 @@
+function validar() {
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let assunto = document.getElementById("assunto").value;
+    let mensagem = document.getElementById("mensagem").value;
+
+    if (nome.trim()==="") {
+        alert("Nome é de preenchimento obrigatorio");
+        return false;        
+    }
+
+    if (email.trim()==="") {
+        alert("E-mail é de preenchimento obrigatorio");
+        return false;        
+    }
+
+    if (assunto.trim()==="") {
+        alert("Selecione um assunto");
+        return false;        
+    }
+
+    if (mensagem.trim()==="") {
+        alert("Escreva uma mensagem");
+        return false;        
+    }
+    
+    return true
+}
+    function verificarOpcao() {
+        let assunto = document.getElementById("assunto").value;
+        let oculta = document.getElementById("oculta");
+
+        if (assunto === "outro") {
+            oculta.style.display = "block";
+            
+        }
+        else{
+            oculta.style.display = "none";
+        }
+
+        
+
+
+        
+    }
+    
